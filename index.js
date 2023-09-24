6f64e50bcc611f832e170e4283ba46124737e55f
Arun MB <ambsdk92@gmail.com>
2023-09-24 17:40:14 +0530

    port changed

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 const express = require("express");
-// const mongoose = require("mongoose");
+const mongoose = require("mongoose");
 // const server = require("./src/app");
 const PORT = process.env.PORT || 4000;
 
@@ -8,15 +8,15 @@ const app = express();
 
 console.log("Establishing database connection...");
 
-// const connectDB = async () => {
-//   try {
-//     const conn = await mongoose.connect(process.env.MONGO_URI);
-//     console.log(`MongoDB Connected: ${conn.connection.host}`);
-//   } catch (error) {
-//     console.log("Mongoose connection error:", error);
-//     process.exit(1);
-//   }
-// };
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.log("Mongoose connection error:", error);
+    process.exit(1);
+  }
+};
 
 // app.use("/api", server);
 
@@ -25,12 +25,12 @@ app.all("/", (req, res) => {
   res.send("changed port");
 });
 
-// connectDB().then(() => {
-//   app.listen(PORT, () => {
-//     console.log("listening for requests");
-//   });
-// });
-
-app.listen(PORT, () => {
-  console.log("listening for requests");
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log("listening for requests");
+  });
 });
+
+// app.listen(PORT, () => {
+//   console.log("listening for requests");
+// });
