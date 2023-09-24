cc43f6b5fb7c2741255cd459e4279f0ab3e0130b
Arun MB <ambsdk92@gmail.com>
2023-09-24 19:22:23 +0530

    added cors

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
 const mongoose = require("mongoose");
 const server = require("./src/app");
 const PORT = process.env.PORT || 4000;
+
+app.use(cors());
 
 const app = express();
 
