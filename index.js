b866e71f898c431f8459ccaec743fbe7792a10a0
Arun MB <ambsdk92@gmail.com>
2023-09-24 17:48:31 +0530

    code clean

--- index.js
+++ index.js
@@ -1,36 +1,27 @@
 require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
-// const server = require("./src/app");
+const server = require("./src/app");
 const PORT = process.env.PORT || 4000;
 
 const app = express();
 
-console.log("Establishing database connection...");
-
 const connectDB = async () => {
+  console.log("Establishing database connection...");
+
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI);
-    console.log(`MongoDB Connected: ${conn.connection.host}`);
+    console.log(`Database Connected: ${conn.connection.host}`);
   } catch (error) {
-    console.log("Mongoose connection error:", error);
+    console.log("Database connection error:", error);
     process.exit(1);
   }
 };
 
-// app.use("/api", server);
-
-app.all("/", (req, res) => {
-  console.log("Just got a request!");
-  res.send("uncommented db conn");
-});
+app.use("/api", server);
 
 connectDB().then(() => {
   app.listen(PORT, () => {
     console.log("listening for requests");
   });
 });
-
-// app.listen(PORT, () => {
-//   console.log("listening for requests");
-// });
