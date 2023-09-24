6b13551831a61cda23ab84f8ac9eb17538137bbe
Arun MB <ambsdk92@gmail.com>
2023-09-24 17:27:51 +0530

    checking mongoose connection

--- index.js
+++ index.js
@@ -1,34 +1,32 @@
-// require("dotenv").config();
+require("dotenv").config();
 const express = require("express");
-// const mongoose = require("mongoose");
+const mongoose = require("mongoose");
 // const server = require("./src/app");
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 
-// console.log("Establishing database connection...");
+console.log("Establishing database connection...");
 
-// const connectDB = async () => {
-//   try {
-//     const conn = await mongoose.connect(process.env.MONGO_URI);
-//     console.log(`MongoDB Connected: ${conn.connection.host}`);
-//   } catch (error) {
-//     console.log(error);
-//     process.exit(1);
-//   }
-// };
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
 
 // app.use("/api", server);
 
 app.all("/", (req, res) => {
   console.log("Just got a request!");
-  res.send("installed express dotenv mongoose");
+  res.send("checking mongoose connection");
 });
 
-app.listen(PORT, () => {
-  console.log("listening for requests");
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log("listening for requests");
+  });
 });
-
-// connectDB().then(() => {
-
-// });
